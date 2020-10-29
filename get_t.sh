6560808dbb9a5295efbc638ff2a71e16ab4edbcb
tcholko <60675323+tcholko@users.noreply.github.com>
2020-10-29 15:55:22 -0700

    Delete get_t.sh

--- get_t.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grep "event" COOe11.log > t_COOe11   #Enter GBD log file name and desired output file name
-
-/home/tim/bin/scripts/kon t_COOe11 105 > kon_COOe11   #Enter number of binders from GBD log
